b64f5f3aae9c5b607eeefa289f170f4f4351a566
OksanaKutsa <kutcaoksana@gmail.com>
2022-09-08 21:43:59 +0100

    created search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="container">
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-footer">
+        Open-source code made by Oksana Kutsa
+      </footer>
+    </div>
+  );
 }
